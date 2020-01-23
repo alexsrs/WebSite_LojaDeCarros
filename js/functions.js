d934b03971d2d707212ed488a5465ace08df1b54
Alex Sandro <alex@creapix.com.br>
2020-01-23 09:31:51 -0300

    formatação em moeda

--- js/functions.js
+++ js/functions.js
@@ -3,7 +3,7 @@ $(function(){
 	var currentValue = 0;
 	var isDrag = false;
 	
-	var preco_maximo = 200000;
+	var preco_maximo = 70000;
 	var preco_atual = 0;
 
 	$('.pointer-barra').mousedown(function() {
@@ -28,14 +28,32 @@ $(function(){
 			$('.pointer-barra').css('left',(mouseX-13)+'px')
 			currentValue = (mouseX /elBase.width()) * 100;
 			$('.barra-preco-fill').css('width',currentValue+'%');
-
-			//TODO : Fazer a formatação do valor em moeda 
 			preco_atual = (currentValue/100)*preco_maximo;
-			$('.preco_pesquisa').html('R$ '+preco_atual);
-
+			//TODO : Fazer a formatação do valor em moeda 
+			preco_atual = formatarPreco(preco_atual);
 			
+			$('.preco_pesquisa').html('R$ '+preco_atual);
 		}
 	})
+
+	function formatarPreco(preco_atual){
+		preco_atual = preco_atual.toFixed(2);
+		preco_arr = preco_atual.split('.');
+		var novo_preco = formatarTotal(preco_arr);
+		return novo_preco;
+	}
+
+	function formatarTotal(preco_arr){
+
+		if(preco_arr[0]<1000){
+			return preco_arr[0]+','+preco_arr[1];
+		} else if(preco_arr[0] < 10000){
+			return preco_arr[0][0]+'.'+preco_arr[0].substr(1,preco_arr[0].lenght)+','+preco_arr[1];
+		} else {
+			return preco_arr[0][0]+preco_arr[0][1]+'.'+preco_arr[0].substr(2,preco_arr[0].lenght)+','+preco_arr[1];
+		}
+
+	}
 
 	function disableTextSelection(){
 		$('body').css("-webkit-user-select","none");
