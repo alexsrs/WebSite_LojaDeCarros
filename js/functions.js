7e97c5067e98ef68c65340a04bb1554321a79c2a
Alex Sandro Ribeiro <117919860@SEAP.RJ>
2020-09-01 14:32:26 -0300

    finalizando front-end

--- js/functions.js
+++ js/functions.js
@@ -138,11 +138,77 @@ $(function(){
 	}
 
 	/* clicar e ir para contato */
+	var directory ='/danki-projetos/projeto_05/'
 
 	$('[goto=contato]').click(function(){
-		$('html,body').animate({scrollTop:$('#contato').offset().top});
+		location.href=directory+'index.html?contato';
 		return false;
 	})
+
+	checkUrl();
+
+	function checkUrl(){
+		var url = location.href.split('/');
+		var curPage = url[url.length-1].split('?');
+
+		if(curPage[1] != undefined && curPage[1] =='contato'){
+			$('header nav a').css('color','black');
+			$('footer nav a').css('color','white');
+			$('[goto=contato]').css('color','#EB2D2D');
+			$('html,body').animate({scrollTop:$('#contato').offset().top});			
+		}
+
+	}
+
+	/* 
+
+	Menu Responsivo
+
+	*/
+
+	$('.mobile').click(function(){
+		$(this).find('ul').slideToggle();
+	})
+
+
+	/* Sistema de navegação dos depoimentos na index */
+
+	var amtDepoimento = $('.depoimento-single p').length;
+	var curIndex =0;
+
+	iniciarDepoimentos();
+	navegarDepoimentos();
+	
+
+	function iniciarDepoimentos(){
+		$('.depoimento-single p').hide();
+		$('.depoimento-single p').eq(0).show();
+	}
+
+	
+
+	function navegarDepoimentos(){
+		$('[next]').click(function(){
+			curIndex++;
+			if(curIndex >= amtDepoimento)
+				curIndex = 0;
+			$('.depoimento-single p').hide();
+			$('.depoimento-single p').eq(curIndex).show();
+
+		})
+
+		$('[prev]').click(function(){
+			
+			curIndex--;
+			if(curIndex < 0)
+				curIndex = amtDepoimento-1;
+			$('.depoimento-single p').hide();
+			$('.depoimento-single p').eq(curIndex).show();
+
+
+		})
+	}
+
 
 
 
