7236cf1c66e263db0657adc73624b61f5e64b6f1
Alex Sandro <alex@creapix.com.br>
2020-09-01 06:15:37 -0300

    atualizando

--- js/functions.js
+++ js/functions.js
@@ -1,7 +1,7 @@
 $(function(){
 
 	var currentValue = 0;
-	var isDrag = false;
+	var isDrag = false; 
 	
 	var preco_maximo = 70000;
 	var preco_atual = 0;
@@ -70,6 +70,79 @@ $(function(){
 		$('body').css("-o-user-select","auto");
 		$('body').css("user-select","auto");
 	}
+
+	/* 
+		mini img => style="background-color: #EB2D2D;" 
+
+		foto-destaque => style="background-image: url(images/carro1.jpg);"
+	*/
+
+	var imgShow = 3;
+	var maxIndex = Math.ceil($('.mini-img-wraper').length/3) - 1;
+	var curIndex = 0;
+
+	initSlider();
+	navigateSlider();
+	clickSlider();
+	
+
+	function initSlider(){
+		var amt = $('.mini-img-wraper').length * 33.3;
+		var elScroll = $('.nav-galeria-wraper');
+		var elSingle = $('.mini-img.wraper');
+		elScroll.css('width',amt+'%');
+		elSingle.css('width',33.3*(100/amt)+'%');
+	}
+
+
+
+	function navigateSlider(){
+		$('.arrow-right-nav').click(function(){
+			if(curIndex < maxIndex){
+				curIndex ++;
+				var elOff = $('.mini-img-wraper').eq(curIndex*3).offset().left - $('.nav-galeria-wraper').offset().left;
+				$('.nav-galeria').animate({'scrollLeft':elOff+'px'});
+
+			} else {
+				console.log("chegamos ate o final");
+			}
+		})
+
+		$('.arrow-left-nav').click(function(){
+			if(curIndex > 0){
+				curIndex --;
+				var elOff = $('.mini-img-wraper').eq(curIndex*3).offset().left - $('.nav-galeria-wraper').offset().left;
+				$('.nav-galeria').animate({'scrollLeft':elOff+'px'});
+
+			} else {
+				console.log("chegamos ate o final");
+			}
+		})
+
+	}
+
+
+	function clickSlider(){
+		$('.mini-img-wraper').click(function(){
+			$('mini-img-wraper').css('background-color','transparent');
+			$(this).css('background-color','rgb(210,210,210)');
+			var img = $(this).children().css('background-image');
+			$('.foto-destaque').css('background-image',img);
+
+
+		})
+
+		$('.mini-img-wraper').eq(0).click();
+
+
+	}
+
+	/* clicar e ir para contato */
+
+	$('[goto=contato]').click(function(){
+		$('html,body').animate({scrollTop:$('#contato').offset().top});
+		return false;
+	})
 
 
 
